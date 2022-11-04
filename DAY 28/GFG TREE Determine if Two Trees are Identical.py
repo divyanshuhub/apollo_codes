c7dbea03e9d8dd87b56a904340f9e2fdf9688c64
Divyanshu Dev Awasthi <66584976+divyanshuhub@users.noreply.github.com>
2022-11-04 12:25:10 +0530

    Create GFG TREE Determine if Two Trees are Identical.py

---
+++ DAY 28/GFG TREE Determine if Two Trees are Identical.py
@@ -0,0 +1,22 @@
+
+'''
+class Node:
+    def _init_(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+class Solution:
+    #Function to check if two trees are identical.
+    def isIdentical(self,root1, root2):
+        # Code here
+        if not root1 and not root2:
+            return True
+        elif root1 and root2:
+            if root1.data==root2.data:
+                if self.isIdentical(root1.left, root2.left):
+                    if self.isIdentical(root1.right, root2.right):
+                        return True
+        else:
+            return False
